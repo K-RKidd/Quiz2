1d4e184ade0a72125167938f67e029fc5e9eb814
K-RKidd <kr248196@muhlenberg.edu>
2014-01-27 10:00:11 -0500

    Initial Commit

---
+++ slider/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  slider
+//
+//  Created by Krystle on 1/27/14.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
